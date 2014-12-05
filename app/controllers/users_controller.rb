6b51f972105343894dc9119f01a687cc49638ee8
cmfreshwater <cmfreshwater@gmail.com>
2014-12-05 22:20:18 +0000

    finish log in and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
-       redirect_to @user 
-    
+      log_in user
+      redirect_to @user 
     else
       render 'new'
     end
